f3ff044156583899227274400da904981569fc70
green-branch <abhinavmehta14@gmail.com>
2019-07-22 17:51:27 -0700

    refactor db initialization code

--- src/test/java/com/amehta/proxy/redis/interact/RedisAppResourceIntegrationTest.java
+++ src/test/java/com/amehta/proxy/redis/interact/RedisAppResourceIntegrationTest.java
@@ -3,15 +3,22 @@ package com.amehta.proxy.redis.interact;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
 
 public class RedisAppResourceIntegrationTest {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(CachedRedisServiceTest.class);
 
     private RedisAppResource resource;
     private JedisPool pool;
@@ -31,10 +38,10 @@ public class RedisAppResourceIntegrationTest {
         */
         pool = new JedisPool(redisAddress, redisPort);
         CachedRedisService cachedRedisService = new CachedRedisService(pool, 2, 1, 1);
-        resource = new RedisAppResource(cachedRedisService, null, 3600); // TODO: read globalExpiry from config
+        resource = new RedisAppResource(cachedRedisService, null, 3600);
         pool = new JedisPool(redisAddress, redisPort);
         try (Jedis resource = pool.getResource()) {
-            resource.set("k1", "v1");
+            resource.set("k1", "v1"); // TODO: set an expiry s.t. this disappers after tests run
         }
         initRedisStore(pool);
     }
@@ -46,14 +53,24 @@ public class RedisAppResourceIntegrationTest {
 
     private void initRedisStore(JedisPool pool) {
         // add key values you would like to initialize DB with
+        // Note that key k1 is reserved for tests. Do not mutate that here
+
+        Map<String, String> preLoadedKeyValues = new HashMap<String, String>() {{
+            // add more key values to meet your requirements
+            put("a1", "b1");
+            put("a2", "b2");
+            put("a3", "b3");
+            put("a4", "b4");
+        }};
         try (Jedis resource = pool.getResource()) {
-            // add more key values
-            // Note that key k1 is reserved for tests. Do not mutate that here
-            resource.set("a1", "b1");
-            resource.set("a2", "b2");
-            resource.set("a3", "b3");
-            resource.set("a4", "b4");
-            resource.set("a4", "b4");
+            for (Map.Entry<String, String> kv : preLoadedKeyValues.entrySet()) {
+                LOGGER.info(format("initializing redis with key=%s value=%s", kv.getKey(), kv.getValue()));
+                String resp = resource.set(kv.getKey(), kv.getValue());
+                if (!"OK".equals(resp))
+                    throw new RuntimeException(format("Bad response=%s while initializing DB", resp));
+                // TODO: 1. read globalExpiry from config 2. check if expiry is set as desired
+                resource.expire(kv.getKey(), 3600);
+            }
         }
     }
 
